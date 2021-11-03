32432de53a8538e24c477861d405e440c893c736
gary yi <gyi@euronetworldwide.com>
2021-11-03 10:59:15 -0500

    date format M/D -> 11/3

--- src/components/DiscColor.vue
+++ src/components/DiscColor.vue
@@ -1,10 +1,10 @@
 <template>
   <div class=container>
-    <div> {{room.patient.name.first || room.patient.name.last ? room.patient.disc.date.format("MM/DD") : '' }}
+    <div> {{room.patient.name.first || room.patient.name.last ? room.patient.disc.date.format("M/D") : '' }}
     </div>
     <select v-model="room.patient.disc.color" :class="room.patient.disc.color"  v-if="room.patient.name.first || room.patient.name.last">
       <option v-for="(v, i) in colors" :key="i" :value="v" class="room-color" :class="v" >
-        {{ room.patient.disc.date.format("MM/DD") }}
+        {{ room.patient.disc.date.format("M/D") }}
       </option>
     </select>
   </div>
